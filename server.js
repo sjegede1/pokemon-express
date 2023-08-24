ed58d34bee47da16b4f540a55dc3b41de1c59175
Sola Jegede <sjegede1@gmail.com>
2023-08-24 01:12:33 -0500

    vercel

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 const Pokemon = require("./models/pokemon");
 
 // =========== MIDDLEWARE ===========
+// app.set('views','/views');
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.json({ extended: false }));
